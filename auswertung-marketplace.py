971f9d751035e15cd56df08f9cebd05119e558e5
Alex <alex.stitz@gmail.com>
2022-11-23 22:05:49 +0100

    auwertung marketplace ergänzt

---
+++ auswertung-marketplace.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+
+# einzelne datei einlesen
+df = pd.read_csv("marketplace-2022-11-23-03-26-03.csv", sep=";")
+
+# durchschnittlichen preis ermitteln
+print(df[df["filename"]])
+
+# niedrigsten preis ermitteln
+
+
+# in einen df hinzufügen
+
+
+# durch alle dateien loopen
+
+
+#prüfen, ob mindestpreis min. xx% unter tages/wochendurchschnitt liegt
+
+
+# email versenden wenn ja
+
+
+# csv mit durchschnitts und mindestwerten aktualisieren
