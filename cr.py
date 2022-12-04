858e2225e6c2100285885da7cf9556c87338ba32
Alex <alex.stitz@gmail.com>
2022-12-04 22:43:30 +0100

    cr.py aktualisiert

--- cr.py
+++ cr.py
@@ -3,6 +3,12 @@ import requests
 import time
 import datetime
 
+def skinwarsanfuegen(df):
+    datum = time.strftime('%Y-%m-%d')
+    df["date"] = datum
+    ausgabe = pd.DataFrame(df.groupby(["name","rarity","date"])["PrizePerSkin"].mean())
+    ausgabe.to_csv("skinwars/skinwars.csv", mode="a", decimal=",", header=False)
+
 filenameMarketplace = 'marketplace-' + time.strftime('%Y-%m-%d-%H-%M-%S') + '.csv'
 
 filenameSkinWars = 'skinwars-' + time.strftime('%Y-%m-%d-%H-%M-%S') + '.csv'
@@ -23,3 +29,4 @@ skinwars = requests.get('https://api.cryptoroyale.one/api/skinwars', params=para
 
 df = pd.DataFrame(skinwars['clans']).transpose()
 df.to_csv('skinwars/' + filenameSkinWars, encoding='utf-8', index=False)
+skinwarsanfuegen(df)
