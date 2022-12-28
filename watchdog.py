1622124d71eb921dd9f7267f827d805e769d6cde
derGrillmeister <112822980+derGrillmeister@users.noreply.github.com>
2022-12-28 11:54:31 +0100

    Update watchdog.py

--- watchdog.py
+++ watchdog.py
@@ -9,7 +9,7 @@ def marketplaceanfuegen(df):
     datum = time.strftime('%Y-%m-%d %H:%M:%S')
     df['date'] = datum
     df['on_sale_price'].astype('float')
-    ausgabe = df.sort_values(['on_sale_price']).groupby('name', as_index=False).first()
+    ausgabe = df.loc[df.groupby('name')['on_sale_price'].idxmin()]
     ausgabe[['rarity','name','date','on_sale_price']].to_csv('watchdog/marketplace.csv', mode='a', decimal=',', header=False, index=False)
 
 filenameMarketplace = 'marketplace-' + time.strftime('%Y-%m-%d-%H-%M-%S') + '.csv'
